53577416e99109b3a807c32a15964caeb4abfa56
amitkumarsingh01 <aksmlibts@gmail.com>
2024-12-09 09:10:21 +0530

    done

---
+++ Ollama1/config.py
@@ -0,0 +1,9 @@
+class Config:
+    PAGE_TITLE = "SafeSpace Chatbot"
+
+    OLLAMA_MODELS = ('llama3.2:latest')
+
+    SYSTEM_PROMPT = f"""You are a helpful medical mental chatbot that has access to the following 
+                    open-source models {OLLAMA_MODELS}.
+                    You can can answer questions for users on any healt, mental health and medical topic. Any other questions not related to medical health medicine u have to reply like please ask medical questions"""
+    
